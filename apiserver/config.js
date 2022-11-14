3b684fc3ce3862f5132c20a597fbc2cbb3a7a4d7
BAlimbekov <alimbekovbekmambet@gmail.com>
2022-11-14 15:54:15 -0600

    modified

---
+++ apiserver/config.js
@@ -0,0 +1,12 @@
+const { Connection } = require("pg");
+module.export = {
+    dev: {
+        connectionString: 'postgresql://postgres:docker@127.0.0.1:5432/workouts_db',
+        port: '3300'
+    },
+    production: {
+        connectionString: process.env.POSTGRES_CONNECTION_STRING + "?ssl=true",
+        port:process.env.PORT
+    }
+
+}
